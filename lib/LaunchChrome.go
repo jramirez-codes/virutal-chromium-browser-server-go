ad292ea797ef321763356a7f1df34d6e394195e3
Jordan Ramirez <jordan.s.ramirez.com>
2025-12-05 10:43:22 -0500

    new logs

--- lib/LaunchChrome.go
+++ lib/LaunchChrome.go
@@ -76,6 +76,8 @@ func LaunchChrome(port int, headless bool) (*ChromeInstance, error) {
 	// Chrome arguments
 	args := []string{
 		fmt.Sprintf("--remote-debugging-port=%d", port),
+		// "--no-sandbox",
+		// "--log-level=3",
 		"--no-first-run",
 		"--no-default-browser-check",
 		"--disable-background-networking",
